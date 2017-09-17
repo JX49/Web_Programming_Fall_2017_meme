be50ffd5dbef282deeeb095f97be71f042e1c1b5
Jack Closs <clossj1@hawkmail.newpaltz.edu>
2017-09-17 15:29:54 -0400

    implementing navbars

--- jquery_mockup/nav.js
+++ jquery_mockup/nav.js
@@ -3,14 +3,14 @@ $(function()
  //9/7   //functions are first class citizens in Jquery/javascript
  //  var hello = "what'sup?" //var allows for functions scope is local to the function
    
-   
-   $("#nav-placeholder").load("_nav.html", function(){
+   $(function(){
+   $("header-placeholder").load("_nav.html", function(){
  
      //list of nav links
-      $(".nav-link").remove("active");
-     $(".nav-link[href=signup.html]").addClass("active");
+      $(".nav-link").removeClass("active");
+      $("#signup-link").addClass("active");
    
-   });
+   } );
 
   
     $(".btn-primary").click(function(){
@@ -22,4 +22,4 @@ $(function()
     
 
 }
-);
+});
