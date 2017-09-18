7e9c2eb44657cebfddcdd87c01d4a35dbd811b5b
Jack Closs <clossj1@hawkmail.newpaltz.edu>
2017-09-18 00:33:02 +0000

    jquery page insertion

--- jquery_mockup/nav.js
+++ jquery_mockup/nav.js
@@ -1,25 +1,15 @@
-$(function()
-{
- //9/7   //functions are first class citizens in Jquery/javascript
- //  var hello = "what'sup?" //var allows for functions scope is local to the function
-   
-   
-   $("#nav-placeholder").load("_nav.html", function(){
- 
-     //list of nav links
-      $(".nav-link").remove("active");
-     $(".nav-link[href=signup.html]").addClass("active");
-   
-   });
 
-  
-    $(".btn-primary").click(function(){
-
-        $(this).closest(".col-md-4").css({'max-width':'none'}).animate({'flexBasis':'100%'})    
-        //closest returns closest parent that matches
-    })
-
+$(function(){
+    $("#header-placeholder").load("_header.html", function () {
+        $(".nav-link").removeClass("active");
+        $("#signup-link").addClass("active");
+        
+    } );
     
 
-}
-);
+    $(".btn-primary").click(function(){
+        $(this).closest(".col-md-4").css({ 'max-width': 'none'})
+            .animate({ 'flex-basis' : '100%' })
+    })
+
+});
